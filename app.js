7149dad72c4a585f003c8ab69a0c06256595b1d9
FredPortela <frederico.veloso@spread.com.br>
2023-03-24 00:42:45 -0300

    Update video player

--- app.js
+++ app.js
@@ -2,28 +2,123 @@ const NodeMediaServer = require('node-media-server');
 require('dotenv').config();
 
 const config = {
-  rtmp: {
+	rtmp: {
     port: 1935,
     chunk_size: 60000,
-    gop_cache: true,
-    ping: 60,
-    ping_timeout: 30
+    gop_cache: false,
+    ping: 30,
+    ping_timeout: 60,
   },
   http: {
     port: 8000,
-	mediaroot: './public',
-    allow_origin: '*'
+    allow_origin: '*',
+    mediaroot: process.env.MEDIA_ROOT || './public',
   },
-  trans: {
-    ffmpeg: process.env.FFMPEG_BIN,
+  relay: {
+    ffmpeg: process.env.FFMPEG_BIN || '/usr/bin/ffmpeg',
     tasks: [
       {
-        app: 'live',
+        app: 'stream',
+        mode: 'push',
+        edge: 'rtmp://127.0.0.1/hls_1080p',
+      },
+      {
+        app: 'stream',
+        mode: 'push',
+        edge: 'rtmp://127.0.0.1/hls_720p',
+      },
+      {
+        app: 'stream',
+        mode: 'push',
+        edge: 'rtmp://127.0.0.1/hls_480p',
+      },
+      {
+        app: 'stream',
+        mode: 'push',
+        edge: 'rtmp://127.0.0.1/hls_360p',
+      },
+    ],
+  },
+  trans: {
+    ffmpeg: process.env.FFMPEG_BIN || '/usr/bin/ffmpeg',
+    tasks: [
+      {
+        app: 'hls_1080p',
+        hls: true,
         ac: 'aac',
-        mp4: true,
-        mp4Flags: '[movflags=faststart]'
-      }
-    ]
+        acParam: ['-b:a', '192k', '-ar', 48000],
+        vcParams: [
+          '-vf',
+          "'scale=1920:-1'",
+          '-b:v',
+          '5000k',
+          '-preset',
+          'fast',
+          '-profile:v',
+          'baseline',
+          '-bufsize',
+          '7500k'
+        ],
+        hlsFlags: '[hls_time=10:hls_list_size=0:hls_flags=delete_segments]',
+      },
+      {
+        app: 'hls_720p',
+        hls: true,
+        ac: 'aac',
+        acParam: ['-b:a', '128k', '-ar', 48000],
+        vcParams: [
+          '-vf',
+          "'scale=1280:-1'",
+          '-b:v',
+          '2800k',
+          '-preset',
+          'fast',
+          '-profile:v',
+          'baseline',
+          '-bufsize',
+          '4200k'
+        ],
+        hlsFlags: '[hls_time=10:hls_list_size=0:hls_flags=delete_segments]',
+      },
+      {
+        app: 'hls_480p',
+        hls: true,
+        ac: 'aac',
+        acParam: ['-b:a', '128k', '-ar', 48000],
+        vcParams: [
+          '-vf',
+          "'scale=854:-1'",
+          '-b:v',
+          '1400k',
+          '-preset',
+          'fast',
+          '-profile:v',
+          'baseline',
+          '-bufsize',
+          '2100k'
+        ],
+        hlsFlags: '[hls_time=10:hls_list_size=0:hls_flags=delete_segments]',
+      },
+      {
+        app: 'hls_360p',
+        hls: true,
+        ac: 'aac',
+        acParam: ['-b:a', '96k', '-ar', 48000],
+        vcParams: [
+          '-vf',
+          "'scale=480:-1'",
+          '-b:v',
+          '800k',
+          '-preset',
+          'fast',
+          '-profile:v',
+          'baseline',
+          '-bufsize',
+          '1200k'
+        ],
+        hlsFlags: '[hls_time=10:hls_list_size=0:hls_flags=delete_segments]',
+      },
+    ],
   }
 };
 
@@ -39,14 +134,28 @@ nms.on('getFilePath', (streamPath, oupath, mp4Filename) => {
   utils.setMp4FilePath(oupath + '/' + mp4Filename);
 });
 
-nms.on('preConnect', (id, args) => {
-  console.log(
-    '[NodeEvent on preConnect]',
-    `id=${id} args=${JSON.stringify(args)}`
-  );
-  // let session = nms.getSession(id);
-  // session.reject();
+nms.on('prePublish', async (id, StreamPath, args) => {
+    let stream_key = getStreamKeyFromStreamPath(StreamPath);
+    console.log('[NodeEvent on prePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+	//console.log('KEY: ',stream_key);
+	/*
+    User.findOne({stream_key: stream_key}, (err, user) => {
+        if (!err) {
+            if (!user) {
+                let session = nms.getSession(id);
+                session.reject();
+            } else {
+                // do stuff
+            }
+        }
+    });
+	*/
 });
+
+const getStreamKeyFromStreamPath = (path) => {
+    let parts = path.split('/');
+    return parts[parts.length - 1];
+};
 
 nms.on('postConnect', (id, args) => {
   console.log(
